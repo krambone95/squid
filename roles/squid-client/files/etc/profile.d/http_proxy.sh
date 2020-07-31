e8e75d4098d0d0943ebfd90e0337cb4ba796cc8f
Mark <mark_stradling@hotmail.com>
2020-07-30 17:37:54 -0700

    Update http_proxy.sh

--- roles/squid-client/files/etc/profile.d/http_proxy.sh
+++ roles/squid-client/files/etc/profile.d/http_proxy.sh
@@ -1,1 +1,12 @@
-export http_proxy='http://proxy.example.com:3128/'
+PROXY_URL="http://192.168.56.131:3128/"
+
+export http_proxy="$PROXY_URL"
+export https_proxy="$PROXY_URL"
+export ftp_proxy="$PROXY_URL"
+export no_proxy="127.0.0.1,localhost"
+
+# For curl
+export HTTP_PROXY="$PROXY_URL"
+export HTTPS_PROXY="$PROXY_URL"
+export FTP_PROXY="$PROXY_URL"
+export NO_PROXY="127.0.0.1,localhost"
