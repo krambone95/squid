c859d8ac77f6d7890a378de31a1fef6f15ef5a8f
Mark <mark_stradling@hotmail.com>
2020-08-05 11:06:50 -0700

    Update http_proxy.sh

--- roles/squid-client/files/etc/profile.d/http_proxy.sh
+++ roles/squid-client/files/etc/profile.d/http_proxy.sh
@@ -3,13 +3,13 @@ PROXY_URL="http://192.168.56.133:3128/"
 export http_proxy="$PROXY_URL"
 export https_proxy="$PROXY_URL"
 export ftp_proxy="$PROXY_URL"
-export no_proxy="127.0.0.1,localhost"
+export no_proxy="127.0.0.1,localhost,*.voleon.net"
 
 # For curl
 export HTTP_PROXY="$PROXY_URL"
 export HTTPS_PROXY="$PROXY_URL"
 export FTP_PROXY="$PROXY_URL"
-export NO_PROXY="127.0.0.1,localhost"
+export NO_PROXY="127.0.0.1,localhost,*.voleon.net"
 
 # for python requests
 export REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-bundle.crt"
